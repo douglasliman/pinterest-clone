e5a10e064c91e1989c15e13606d022e3902fbcc0
Douglas Lima <douglas.lima.queiroz@gmail.com>
2023-08-24 22:00:52 -0300

    [UP] - functions-for-animation

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
 const grids = document.querySelectorAll('.grid')
 const headings = document.querySelectorAll('.heading .wrapper .text')
 
-function enterScreen(i){
-    const grid = grids[i]
-    const heading = headings[i]
-const gridColumns = grid.querySelectorAll('.column')
+function enterScreen(index) {
+  const grid = grids[index]
+  const heading = headings[index]
+  const gridColumns = grid.querySelectorAll('.column')
 
-grid.classList.add('active')
+  grid.classList.add('active')
 
-
-gridColumns.forEach(element => {
-    element.classList.remove('animate-before')
+  gridColumns.forEach(element => {
+    element.classList.remove('animate-before', 'animate-after')
   })
 
+  heading.classList.remove('animate-before', 'animate-after')
 }
 
+function exitScreen(index, exitDelay) {
+  const grid = grids[index]
+  const heading = headings[index]
+  const gridColumns = grid.querySelectorAll('.column')
 
-function outScreen(i, delay){}
+  gridColumns.forEach(element => {
+    element.classList.add('animate-after')
+  })
 
+  heading.classList.add('animate-after')
 
-function setupAnimationCycle({
-    initialScreenIndex,
-     timePerScreen, 
-     delay
-}){
-    enterScreen(initialScreenIndex)
+  setTimeout(() => {
+    grid.classList.remove('active')
+  }, exitDelay)
 }
+
+function setupAnimationCycle({ timePerScreen, exitDelay }) {
+  const cycleTime = timePerScreen + exitDelay
+  let nextIndex = 0
+
+  function nextCycle() {
+    const currentIndex = nextIndex
+
+    enterScreen(currentIndex)
+
+    setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen)
+
+    nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1
+  }
+
+  nextCycle()
+
+  setInterval(nextCycle, cycleTime)
+}
+function photosOnSquares() {
+    const items = document.querySelectorAll(".item");
+
+    items.forEach(function(item) {
+        fetch("https://source.unsplash.com/random")
+            .then(response => response.url)
+            .then(url => {
+                item.style.backgroundImage = `url(${url})`;
+            })
+            .catch(error => console.error(error));
+    });
+    
+}
+photosOnSquares() 
 
 setupAnimationCycle({
-    initialScreenIndex:0,
-    timePerScreen:2000, //ms
-    delay: 200 * 7,
+  timePerScreen: 2000, // ms
+  exitDelay: 200 * 7 // ms
 })
